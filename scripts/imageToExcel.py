dfc673e1d2e29aaa3ffc43008e154a98c7c84780
elishatofunmi <elishatofunmi@gmail.com>
2020-09-08 02:18:52 -0400

    commit

---
+++ scripts/imageToExcel.py
@@ -0,0 +1,20 @@
+import cv2
+import pytesseract
+import numpy as np
+import os, sys
+import pandas as pd
+
+
+os.chdir('/home/odemakinde/Desktop/Image to Excel/Image-to-Excel/test images/')
+img = cv2.imread('menu2.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+gray = cv2.bitwise_not(img_bin)
+cv2.imshow('image', gray)
+
+
+kernel = np.ones((2, 1), np.uint8)
+img = cv2.erode(gray, kernel, iterations=1)
+img = cv2.dilate(img, kernel, iterations=1)
+out_below = pytesseract.image_to_string(img)
+print("OUTPUT:", out_below)
